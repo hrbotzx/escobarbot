35987966852b716f13a30a450417209038cf90dd
hrbotzx <henriquemaurerkunz@gmail.com>
2021-02-17 13:52:16 -0300

    First commit

---
+++ src/escobarmenu.js
@@ -0,0 +1,12 @@
+const escobarmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Escobar*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.escobarmenu = escobarmenu
